d68ce2c076061f76e1392abf0e10cf02c879073e
emm2305 <50798469+emm2305@users.noreply.github.com>
2020-03-12 06:49:16 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ controller.up.onEvent(ControllerButtonEvent.Released, function () {
     if (answerOn == 1) {
         choice = 0
         mySprite.setImage(arrows[choice])
+        music.ringTone(notesList[Math.randomRange(0, 6)])
         checkChoice()
     }
 })
@@ -25,12 +26,14 @@ controller.left.onEvent(ControllerButtonEvent.Released, function () {
     if (answerOn == 1) {
         choice = 3
         mySprite.setImage(arrows[choice])
+        music.ringTone(notesList[Math.randomRange(0, 6)])
         checkChoice()
     }
 })
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
     if (answerOn == 1) {
         choice = 1
+        music.ringTone(notesList[Math.randomRange(0, 6)])
         mySprite.setImage(arrows[choice])
         checkChoice()
     }
@@ -59,17 +62,18 @@ function Level_Over () {
     Timer = (game.runtime() - Start_time) / 1000
     list.push(points)
     game.splash("" + Math.round(points / Timer * 100) + " " + "points")
-    whichLevel += 1
+    pause(2000)
+    game.reset()
 }
 controller.right.onEvent(ControllerButtonEvent.Released, function () {
     if (answerOn == 1) {
         choice = 2
+        music.ringTone(notesList[Math.randomRange(0, 6)])
         mySprite.setImage(arrows[choice])
         checkChoice()
     }
 })
 function MusicLevel () {
-    notesList = [262, 294, 330, 392, 440, 523, 220]
     game.showLongText("Carefully watch the arrows,remember the order in which they appear", DialogLayout.Center)
     for (let index = 0; index < 7; index++) {
         ChooseArrow = Math.randomRange(0, 3)
@@ -94,19 +98,21 @@ function MusicLevel () {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `)
+        music.ringTone(notesList[Math.randomRange(0, 6)])
         pause(100)
     }
     music.stopAllSounds()
     console.log(arrowList)
     points = 0
     game.showLongText("Use the keypad to recreate the pattern", DialogLayout.Center)
-    answerOn = 2
+    answerOn = 1
     Start_time = game.runtime()
+    music.ringTone(notesList[Math.randomRange(0, 6)])
 }
 let ChooseArrow = 0
-let notesList: number[] = []
 let Start_time = 0
 let choice = 0
+let notesList: number[] = []
 let answerOn = 0
 let Timer = 0
 let points = 0
@@ -209,4 +215,5 @@ points = 0
 Timer = 0
 let whichLevel = 0
 answerOn = 0
+notesList = [262, 294, 330, 392, 440, 523, 220]
 MusicLevel()
